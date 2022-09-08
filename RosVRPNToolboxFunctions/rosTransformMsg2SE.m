9d0f80d634baecfa9e4b534fc47b5f8392034cc9
hfhelmich <hfhelmich@gmail.com>
2022-09-08 12:30:37 -0400

    Moving messages to new toolbox

--- RosVRPNToolboxFunctions/rosTransformMsg2SE.m
+++
@@ -1,7 +0,0 @@
-function out = rosTransformMsg2RBT(msg)
-% ROSTRANSFORMMSG2RBT converts a ROS Transform message to a 4x4 Rigid Body
-    translation = msg.Transforms.Transform.Translation;
-
-
-
-end
